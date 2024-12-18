015b23a931cdccc3f8866b3769b4a7793dee7d1d
Adea Piperku <ap64975@ubt-uni.net>
2024-12-18 23:48:08 +0100

    App and Home files

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useContext, useState } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import { queryClient, setQueryDefaults } from "./service/QueryClient";
+import { QueryClientProvider } from "react-query";
+import UserContext from "./context/UserContext";
+import AppRoutes from "./routes/Routes";
+import DateFnsUtils from '@date-io/date-fns';
+import { Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { CartProvider } from "./context/CartContext";
+import DarkMode from "./context/DarkMode";
+
+const customTheme = {
+  overrides: {
+    MuiTableRow: {
+      head: {
+        background:
+          "linear-gradient(90deg, rgba(191,16,0,1) 0%, rgba(209,9,9,1) 28%, rgba(227,99,35,1) 58%, rgba(255,250,37,1) 100%)",
+        color: "white",
+      },
+    },
+    MuiTableSortLabel: {
+      root: {
+        color: "yellow",
+        fontSize: "1.2em",
+        "&:hover": {
+          color: "#424242 !important",
+        },
+        "&.MuiTableSortLabel-active": {
+          color: "#121212",
+        },
+        "& *": {
+          color: "#2f2f2f !important",
+        },
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: "#DB0007",
+      mainGradient:
+        "linear-gradient(90deg, rgba(191,16,0,1) 0%, rgba(209,9,9,1) 28%, rgba(227,99,35,1) 58%, rgba(255,250,37,1) 100%)",
+    },
+    secondary: {
+      main: "#FFBC0D",
+    },
+    text: {
+      dark: "#121212",
+    },
+    type: "light",
+  },
+  toolbarHeight: 50,
+};
+
+setQueryDefaults();
 
 function App() {
+  const [theme, setTheme] = useState(customTheme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <CartProvider>
+        <QueryClientProvider client={queryClient}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <ThemeProvider theme={createTheme(theme)}>
+          <DarkMode.Provider value={{theme, setTheme}}>
+            <CssBaseline />
+            <Routes>{AppRoutes}</Routes>
+            </DarkMode.Provider>
+          </ThemeProvider>
+          </MuiPickersUtilsProvider>
+        </QueryClientProvider>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
